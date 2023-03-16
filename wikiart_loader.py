f16140fc72299be67821389b9d7a825734901106
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-03-16 15:10:57 +0100

    Debug

--- wikiart_loader.py
+++ wikiart_loader.py
@@ -20,7 +20,8 @@ class WikiArtLoader(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 1])
+        print(self.img_labels.iloc[idx])
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
 
         image = read_image(img_path)
         label = self.img_labels.iloc[idx, 1]
