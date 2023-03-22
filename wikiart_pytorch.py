1293b04813898b419b8ae8ef56d52d5f4fbb6e2a
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-03-22 16:34:11 +0100

    Update wikiart_pytorch.py

--- wikiart_pytorch.py
+++ wikiart_pytorch.py
@@ -82,9 +82,9 @@ def train(epochs):
 
             # Print statistics
             running_loss += loss.item()
-            if i % 50 == 0:    # print every 50 mini-batches
+            if i % 10 == 0:    # print every 50 mini-batches
                 print('Loss: %.3f, Batch %d / %d' %
-                        (running_loss / len(train_dataloader), i, int(len(train_data) / args.batch_size)))
+                        (running_loss / len(train_data), i, int(len(train_data) / args.batch_size)))
                 
         running_loss = 0.0
 
