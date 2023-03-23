635d13a09404312082985c6b45c1fbebd4981f14
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-03-23 14:46:39 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,13 +1,13 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
 parser.add_argument('--epochs', type=int, default=300, help='Number of epochs to train')
 parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum')
 parser.add_argument('--no_cuda', action='store_true', default=False, help='Disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, help='Random seed')
-parser.add_argument('--model_path', type=str, default='model.pt', help='Path to save the model')
 parser.add_argument('--data_path', type=str, default='data', help='Path to the data')
 parser.add_argument('--train_file', type=str, default='train.csv', help='Path to the train file')
 parser.add_argument('--val_file', type=str, default='test.csv', help='Path to the val file')
